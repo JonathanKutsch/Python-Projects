aa212e3a9d9e1f6d47ed4b8eee747f2f46d89d1c
Jonathan Kutsch <66543465+JonathanKutsch@users.noreply.github.com>
2020-12-18 19:24:32 -0600

    Quiz 1: sequential steps/variables

---
+++ Quizzes/Quiz1/main.py
@@ -0,0 +1,27 @@
+# By submitting this assignment, I agree to the following:
+#  “Aggies do not lie, cheat, or steal, or tolerate those who do”
+#  “I have not given or received any unauthorized aid on this assignment”
+#
+# Name:           Jonathan Kutsch
+# Section:        514
+# Team:           n/a
+# Assignment:     Quiz 1 - Sequential Steps, Variables, Assignments, Output
+# Date:           18 September 2020
+
+from math import *
+
+length = 7.23
+area = 7.23 ** 2
+perimeter = 4 * 7.23
+print("The area of a square with a side length of 7.23 is", area)
+print("The perimeter of a square with a side length of 7.23 is", perimeter)
+# area of square = side * side
+# perimeter of square = 4 * side
+
+radius = sqrt(area / pi)
+print("The radius of a circle with same area is", radius)
+# radius of circle = square root of (area divided by pi)
+
+perimeter_triangle = 3 * radius
+print("The perimeter of an equilateral triangle with circle radius being side length is", perimeter_triangle)
+# perimeter of equilateral triangle = 3 * side
