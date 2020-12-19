238432df38b95a147efa1210a6e68123342ea769
Jonathan Kutsch <66543465+JonathanKutsch@users.noreply.github.com>
2020-12-18 19:13:11 -0600

    print statements

---
+++ Labs/Lab01/Math Wall Clock.py
@@ -0,0 +1,23 @@
+# By submitting this assignment, I agree to the following:
+#  �Aggies do not lie, cheat, or steal, or tolerate those who do�
+#  �I have not given or received any unauthorized aid on this assignment�
+#
+# Name:         Jonathan Kutsch
+# Section:      514
+# Assignment:   Lab 01 Wall Clock
+# Date:         22 August 2020
+
+from math import *
+
+print(cos(0))
+print(96 + 50 - 144)
+print(27 % 6)
+print(sqrt(64) - sqrt(16))
+print((4000 + 1000) / (250 * 4))
+print(sin(0) + 2 ** 2 + 12 % 5)
+print(tan(pi / 3) - tan(2 * pi / 6) + sqrt(49))
+print(2 / exp(1) + sqrt(64) - .735758882342886)
+print(2 ** 7 - fabs(-2) - 9 * 13)
+print(log10(10000) + sqrt(36))
+print(550 // 45 - 1)
+print(100 % 3 + 4 ** 2 - 30 / 6)
