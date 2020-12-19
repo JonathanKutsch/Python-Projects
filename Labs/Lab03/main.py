92950f614ea5d9ed7684d44d863fcc85595c162e
Jonathan Kutsch <66543465+JonathanKutsch@users.noreply.github.com>
2020-12-18 19:15:06 -0600

    user input

---
+++ Labs/Lab03/main.py
@@ -0,0 +1,9 @@
+# individual part of Lab 03 - Activity 3
+
+Name = input('Type a Name: ')
+Noun = input('Type a Noun: ')
+Location = input('Type a Location: ')
+Color = input('Type a Color: ')
+Number = input('Type a Number: ')
+
+print("\bMy mother\'s name is {0}\n{1} is my favorite toy\n\tHeaven looks like {2}\n\\\Your {3} dog is adorable and so are you and your {4} kittens".format(Name, Noun, Location, Color, Number))
