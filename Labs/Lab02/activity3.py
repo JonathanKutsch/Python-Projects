58b3a0ff1a6747dc5282defc5831fbb3fdab0871
Jonathan Kutsch <66543465+JonathanKutsch@users.noreply.github.com>
2020-12-18 19:14:12 -0600

    variables

---
+++ Labs/Lab02/activity3.py
@@ -0,0 +1,14 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do"
+# "I have not given or received any unauthorized aid on this assignment"
+#
+# Name: Team 21: Eric Love, Jonathan Kutsch, Andrew Krecek, Caeden Landry
+# Section: 514
+# Assignment: Lab02
+# Date: 26 8 2020
+changeOft = 45 - 30
+changeOfd = 615 - 50
+m = changeOfd / changeOft
+t = 37
+d = 50 + m * (t-30)
+print(d)
